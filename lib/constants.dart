1651869c13557075ae8f59c4130cd8b306b905dc
Ittipat Pattum <nampu@finema.co>
2024-04-30 19:55:30 +0700

    add key

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 const int nullAliasInt = 0;
 const String nullAliasString = 'null';
 const String keyToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHA3ODlwbGF0ZXMiLCJzdWIiOiIxNiIsImV4cCI6MTcxMzU0MDM5NywiaWF0IjoxNzEyMzMwNzk3fQ.bXU1LaEGV7oH4YA3Kqtqsb5LydWQAgRHZiJpg30nd24';
+const String apiKey = 'A46442B3454C93571153AB45137A2';
 
 const FlutterSecureStorage flutterSecureStorage = FlutterSecureStorage();
